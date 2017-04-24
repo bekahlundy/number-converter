3a46c4f508fa7889ca7f01cb9c4728573f281204
bekahlundy <lundyrebekah@gmail.com>
2017-04-24 08:43:06 -0600

    Update test description

--- test/numberConverter-test.js
+++ test/numberConverter-test.js
@@ -9,7 +9,7 @@ describe('numberConverter', () => {
   it('returns twenty-three billion, four hundred fifty-six million, ninety-eight thousand, three hundred twenty-five', () => {
     assert.deepEqual(numberConverter(23456098325), 'twenty-three billion, four hundred fifty-six million, ninety-eight thousand, three hundred twenty-five')
   })
-  it('returns Two thousand five hundred twenty-three and 04/100', () => {
+  it('returns two thousand, five hundred twenty-three and 04/100', () => {
     assert.deepEqual(numberConverter(2523.04), 'two thousand, five hundred twenty-three and 04/100')
   })
 
