55cfec798a1e848aac48e2defa5a5cb18e273b30
bekahlundy <lundyrebekah@gmail.com>
2017-04-23 22:19:12 -0600

    Complete code challenge

---
+++ test/numberConverter-test.js
@@ -0,0 +1,16 @@
+const assert = require('chai').assert;
+const { numberConverter } = require('../lib/numberConverter.js');
+
+describe('numberConverter', () => {
+
+  it('returns one hundred', () => {
+    assert.deepEqual(numberConverter(100), 'one hundred ')
+  })
+  it('returns twenty-three billion, four hundred fifty-six million, ninety-eight thousand, three hundred twenty-five', () => {
+    assert.deepEqual(numberConverter(23456098325), 'twenty-three billion, four hundred fifty-six million, ninety-eight thousand, three hundred twenty-five')
+  })
+  it('returns Two thousand five hundred twenty-three and 04/100', () => {
+    assert.deepEqual(numberConverter(2523.04), 'two thousand, five hundred twenty-three and 04/100')
+  })
+
+});
